d9b0ccfc4254200887ab8348bfa66d6e4bd0c83d
DESKTOP-PU6BDL2\hi420 <hi17sawano@g.kumamoto-nct.ac.jp>
2020-12-01 16:51:26 +0900

    簡単なサーバクライアントを実装しました。

--- client/client/MainWindow.xaml.cs
+++ client/client/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Net.Sockets;
 
 namespace client
 {
@@ -27,7 +16,89 @@ namespace client
 
 		private void StartButton_Click(object sender, RoutedEventArgs e)
 		{
+			my_client.Connect("127.0.0.1","Hello!");
 			MessageBox.Show("start");
 		}
+		public void ShowError(Exception e)
+		{
+			MessageBox.Show(e.Message);
+		}
+
+		private void stopButton_Click(object sender, RoutedEventArgs e)
+		{
+			my_client mine = new my_client();
+
+			mine.sentMessage("stop");
+		}
 	}
+
+	public class my_client
+	{
+		//TcpClient tcpCliant;
+		private static NetworkStream stream;
+		//private string adress = "127.0.0.1";
+		//private Int32 port = 2001;
+		public static void Connect(String server, String message)
+		{
+			my_client mine = new my_client();
+			try
+			{
+				// Create a TcpClient.
+				// Note, for this client to work you need to have a TcpServer
+				// connected to the same address as specified by the server, port
+				// combination.
+				Int32 port = 2001;
+				TcpClient client = new TcpClient(server, port);
+
+				// Get a client stream for reading and writing.
+
+				stream = client.GetStream();
+
+				// Send the message to the connected TcpServer.
+
+				mine.sentMessage(message);
+
+				
+
+				Console.WriteLine("Sent: {0}", message);
+
+				// Receive the TcpServer.response.
+
+				// Buffer to store the response bytes.
+				Byte[] data = new Byte[256];
+
+				// String to store the response ASCII representation.
+				String responseData = null;
+
+				// Read the first batch of the TcpServer response bytes.
+				Int32 bytes = stream.Read(data, 0, data.Length);
+				responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+				Console.WriteLine("Received: {0}", responseData);
+
+				// Close everything.
+				stream.Close();
+				client.Close();
+			}
+			catch (ArgumentNullException e)
+			{
+				Console.WriteLine("ArgumentNullException: {0}", e);
+			}
+			catch (SocketException e)
+			{
+				Console.WriteLine("SocketException: {0}", e);
+			}
+		}
+		public int sentMessage(string message) {
+			// Translate the passed message into ASCII and store it as a Byte array.
+			Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+			MessageBox.Show("Send :"+message) ;
+			if (message == "stop")
+				return 1;
+			stream.Write(data, 0, data.Length);
+			return 0;
+		}
+
+	}
+
+	
 }
--- server/server/MainWindow.xaml.cs
+++ server/server/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Net;
 
 namespace server
 {
@@ -24,5 +26,66 @@ namespace server
 		{
 			InitializeComponent();
 		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Wait Start.");
+			my_server.wait();
+		}
+	}
+	class my_server {
+
+		public static void wait()
+		{
+			TcpListener listener = null;
+			try
+			{
+				//自身に接続するように設定
+				Int32 port = 2001;
+				IPAddress address = IPAddress.Parse("127.0.0.1");
+
+				//サーバーの初期化
+				listener = new TcpListener(address, port);
+
+				//リクエストを受け付け開始
+				listener.Start();
+
+				Byte[] bytes = new Byte[256];
+				string data = null;
+
+				while (true)
+				{
+
+					TcpClient tcpClient = listener.AcceptTcpClient();
+					MessageBox.Show("Connected.");
+
+					data = null;
+					NetworkStream stream = tcpClient.GetStream();
+					int i;
+					while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+					{
+						data = Encoding.ASCII.GetString(bytes, 0, i);
+						MessageBox.Show("Received :" + data);
+						data = data.ToUpper();
+
+						byte[] msg = Encoding.ASCII.GetBytes(data);
+						stream.Write(msg, 0, msg.Length);
+						MessageBox.Show("Sent : " + msg);
+					}
+					tcpClient.Close();
+				}
+
+
+			}
+			catch (SocketException e)
+			{
+				MessageBox.Show(e.Message);
+			}
+			finally {
+				listener.Stop();
+			}
+
+		}
 	}
 }
+
